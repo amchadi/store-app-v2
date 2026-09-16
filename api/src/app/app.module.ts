9c377a48f9a2d7f3cec85959a1709989907f69ca
Med Adnane Abouchadi <adnaneabouchadi@gmail.com>
2026-09-16 20:42:30 +0100

    feat(api): configure backend environment variables

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { envValidationSchema } from './config/env.validation';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envValidationSchema,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
